a207e8ec8c7739ac7c642de986ce18d4269f725e
Grey123e <g.donchyk@gmail.com>
2023-04-29 22:54:55 -0500

    commit123

--- src/greycourse5/Class1.java
+++ src/greycourse5/Class1.java
@@ -3,8 +3,24 @@ package greycourse5;
 public class Class1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world! ");
+		
+		
+		int a = 10;
+		int b = 20;
+		
 
+		System.out.println("Before swap");
+		System.out.println("a is " + a);
+		System.out.println("b is " + b);
+		
+		a = a + b; // a is 30 b is 20
+		b = a - b; // b is 10 now and a is 30
+		a = a - b;  // b is 10 now and a is 20 now
+		
+		
+		System.out.println("After swap");
+		System.out.println("a is " + a);
+		System.out.println("b is " + b);
 	}
 
 }
