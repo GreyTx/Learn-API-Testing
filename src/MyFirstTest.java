66285d895eac33ce37a6017be9903255ccf0ff47
Grey123e <g.donchyk@gmail.com>
2023-09-18 20:04:06 -0500

    commit using git bash

--- src/MyFirstTest.java
+++ src/MyFirstTest.java
@@ -7,6 +7,8 @@ public class MyFirstTest {
         System.out.println("Hello world!");
         System.out.println("Hello world2!");
         System.out.println("This is group 2 first test case");
+        System.out.println("This is group 2 second test case");
+
     }
 
 }
