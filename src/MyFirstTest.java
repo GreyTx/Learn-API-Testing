7070fd6512d07651bddee1377bafedcf0beded16
Grey123e <g.donchyk@gmail.com>
2023-09-18 19:53:59 -0500

    first test added

--- src/MyFirstTest.java
+++ src/MyFirstTest.java
@@ -4,6 +4,9 @@ public class MyFirstTest {
         System.out.println("Hello world!");
         System.out.println("Hello world2!");
         System.out.println("Hello !");
+        System.out.println("Hello world!");
+        System.out.println("Hello world2!");
+        System.out.println("This is group 2 first test case");
     }
 
 }
