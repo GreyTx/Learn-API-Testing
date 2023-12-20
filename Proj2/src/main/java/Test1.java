c756431b94ab4b5fa8da9a4c19006f508001ffbb
GreyTx <82722264+GreyTx@users.noreply.github.com>
2023-12-20 14:27:55 -0600

    Update Test1.java
    2nd test cases added

--- Proj2/src/main/java/Test1.java
+++ Proj2/src/main/java/Test1.java
@@ -3,4 +3,9 @@ public class Test1 {
     public static void testCase123() {
         System.out.println("Test case passed");
     }
+
+        public static void testCase574() {
+        System.out.println("Sub test Test case failed");
+    }
+    
 }
