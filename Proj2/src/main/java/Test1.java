5f55be8ff070119efbf2e1530deb8ab377e45c49
Grey123e <g.donchyk@gmail.com>
2023-12-20 14:28:12 -0600

    2nd test case added

--- Proj2/src/main/java/Test1.java
+++ Proj2/src/main/java/Test1.java
@@ -3,4 +3,8 @@ public class Test1 {
     public static void testCase123() {
         System.out.println("Test case passed");
     }
+
+    public static void testCase123SubTets() {
+        System.out.println("Sub test Test case passed");
+    }
 }
