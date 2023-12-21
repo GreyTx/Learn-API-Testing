9e058f16e083fe9e0a447057044f2f4006a07a05
Grey123e <g.donchyk@gmail.com>
2023-12-20 20:20:20 -0600

    commit with a new variable

--- Proj2/src/main/java/TestCase123.java
+++ Proj2/src/main/java/TestCase123.java
@@ -3,3 +3,6 @@ public class TestCase123 {
         //logic here
     }
 }
+
+
+//flow:   you create a feature branch (check out a branch). you write you code. you commit and push your code. you create PR. your test case code is reviewed by you test lead. then your code is merged with master branch.
