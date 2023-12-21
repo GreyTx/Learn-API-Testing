add7cf6fa1f73625586acf0d48a140f82b6dd451
Grey123e <g.donchyk@gmail.com>
2023-12-20 20:32:20 -0600

    method2 added

--- Proj2/src/main/java/Main.java
+++ Proj2/src/main/java/Main.java
@@ -2,19 +2,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) {
 
 
-        String name = "Grey";
-       int b = 10;
-
-
-        System.out.println("Hello world!");
-
-
-
-
-}
+    private void method2(String str) {
+        //some logic here
+    }
 
 
 
