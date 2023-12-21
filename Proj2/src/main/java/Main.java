0833ccd70e71d09c915ec4640af94c56d0be512b
Grey123e <g.donchyk@gmail.com>
2023-12-20 20:04:09 -0600

    Commit with error

--- Proj2/src/main/java/Main.java
+++ Proj2/src/main/java/Main.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) {
+
 
 
         System.out.println("Hello world!");
@@ -11,14 +11,6 @@ public class Main {
 
 
 
-
-
-
-
-
-
-
-}
 
 
 
